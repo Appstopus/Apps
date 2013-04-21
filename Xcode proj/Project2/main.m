db4bef247c908ca51390cca15e3177e6016537eb
Khee Men <kheemen@hotmail.com>
2013-04-21 15:14:35 +0800

    commit test

---
+++ Xcode proj/Project2/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Project2
+//
+//  Created by Shirly Cheng on 21/3/13.
+//  Copyright (c) 2013 Hyersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+    //comment test 5
+    //commit2 comment4
+}
